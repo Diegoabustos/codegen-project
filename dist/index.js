581028260b946a0e2d57ba37cb19182fd955009d
Diego <diegoabustos24@gmail.com>
2022-05-04 13:27:14 -0500

    init project

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const world = "world";
+function hello(word) {
+    console.log(`Hello ${word}! `);
+}
+exports.hello = hello;
+hello(world);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const world = "world";export function hello(word: string): void {
+    console.log(`Hello ${word}! `);
+}hello(world);
