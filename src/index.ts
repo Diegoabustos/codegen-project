bf9f45a0beb4e479d068deceeb5f44cedc359ab0
Diego <diegoabustos24@gmail.com>
2022-05-05 10:22:37 -0500

    add schemas and resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
-import { Todo } from "./types/types";const todos: Todo[] = [
-    {
-      title: "Walk dog",
-      description: "Before 9am",
-    },
-    {
-      title: "Clean bathroom",
-      description: "",
-    },
-  ];
+import { Todo } from "./types/types";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { ApolloServer } from "apollo-server";
+
+const schema = loadSchemaSync("**/*.graphql", {
+  loaders: [new GraphQLFileLoader()],
+});
+
+const resolvers = {
+  Query: {
+    todos: (): Todo[] => todos,
+  },
+};
+
+const schemaWithResolvers = addResolversToSchema({
+  schema,
+  resolvers,
+});
+
+const server = new ApolloServer({ schema: schemaWithResolvers });server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+
+const todos: Todo[] = [
+  {
+    title: "Walk dog",
+    description: "Before 9am",
+  },
+  {
+    title: "Clean bathroom",
+    description: "",
+  },
+];
